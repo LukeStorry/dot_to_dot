659fa0c9cde01b5773f01cae560c4ea701ea2298
Luke Storry <Luke@LukeStorry.co.uk>
2016-10-19 15:40:23 +0100

    improved drawing with buttons

--- dtd.py
+++ dtd.py
@@ -1,32 +1,51 @@
 import Tkinter
 
 
-oldx, oldy = 0, 0  # need to actually move cursor here - platform depedant
-list_of_dots = []
-
-
-def initalise_dots():
+def choose_input(root):
     pass  # TODO
 
 
-def motion(event):
-    global oldx, oldy, list_of_dots
-    print event.x, event.y
-    event.widget.create_line(oldx, oldy, event.x, event.y, smooth=True)
-    oldx, oldy = event.x, event.y
+def initalise_dots(root):
+    pass  # TODO
 
-    if (event.x, event.y) in list_of_dots:
-        pass  # TODO
+
+oldx, oldy = None, None
+mb = "up"
+
+
+def initialise_canvas():
+    root = Tkinter.Tk()
+    canvas = Tkinter.Canvas(root, bg="white", height=800, width=1000)
+    canvas.pack()
+    canvas.bind("<Motion>", motion)
+    canvas.bind("<ButtonPress-1>", mouse_button_press)
+    canvas.bind("<ButtonRelease-1>", mouse_button_release)
+    root.mainloop()
+
+
+def mouse_button_press(event):
+    global mb
+    mb = "down"
+
+
+def mouse_button_release(event):
+    global mb, oldx, oldy
+    mb = "up"
+    oldx, oldy = None, None
+
+
+def motion(event):
+    if mb == "down":
+        global oldx, oldy
+        if oldx is not None and oldy is not None:
+            event.widget.create_line(oldx, oldy, event.x, event.y, smooth=True)
+        oldx, oldy = event.x, event.y
 
 
 def main():
-    top = Tkinter.Tk()
-    frame = Tkinter.Canvas(top, bg="white", height=500, width=800)
-    top.event_generate("<Motion>", warp=True, x=0, y=0)  # only works in linux
-    frame.bind("<Motion>", motion)
-    frame.pack()
-    top.mainloop()
-
+    initialise_canvas()
+    choose_input()
+    initalise_dots()
 
 if __name__ == "__main__":
     main()
