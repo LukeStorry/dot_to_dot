75ff185ce9d1a3af84c8f1e0129ca25344e653b5
Luke Storry <Luke@LukeStorry.co.uk>
2016-11-05 18:47:26 +0000

    moved everything to main - too many globals

--- dtd.py
+++ dtd.py
@@ -1,38 +1,8 @@
 import Tkinter
+import time
 
 precision = 5
 oldx, oldy = None, None
-mb = "up"
-list_of_dots = {(50, 50): False, (100, 100): False,
-                (100, 50): False, (50, 100): False, }
-canvas, root = None, None
-
-
-def choose_input():
-    pass  # TODO
-
-
-def draw_dots():
-    global list_of_dots, precision, canvas
-    for (x, y) in list_of_dots:
-        canvas.create_oval(x - precision, y - precision, x + precision,
-                           y + precision, fill="blue")
-    canvas.pack()
-
-
-def initialise_canvas():
-    global canvas, root
-    root = Tkinter.Tk()
-    canvas = Tkinter.Canvas(root, bg="white", height=800, width=1000)
-    canvas.pack()
-
-
-def start_game():
-    global canvas, root
-    canvas.bind("<Motion>", motion)
-    canvas.bind("<ButtonPress-1>", mouse_button_press)
-    canvas.bind("<ButtonRelease-1>", mouse_button_release)
-    root.mainloop()
 
 
 def mouse_button_press(event):
@@ -70,20 +40,31 @@ def hit_pixel(x, y):
     pass  # TODO: add to hitlist?
 
 
-def write_results_to_file():
-    with open("results.csv", "a") as myfile:
-        myfile.write("results_test")
-
-
 def main():
-    initialise_canvas()
-    print 1
-    draw_dots()
-    print 2
-    # choose_input() ?
-    start_game()
-    print 3
-    write_results_to_file()
+    mb = "up"
+    list_of_dots = {(50, 50): False, (100, 100): False,
+                    (100, 50): False, (50, 100): False, }
+
+    while(True):
+        # set up tkinter
+        root = Tkinter.Tk()
+        canvas = Tkinter.Canvas(root, bg="white", height=800, width=1000)
+
+        # draw dots
+        for (x, y) in list_of_dots:
+            canvas.create_oval(x - precision, y - precision, x + precision,
+                               y + precision, fill="blue")
+        canvas.pack()
+
+        # bind inputs
+        canvas.bind("<Motion>", motion)
+        canvas.bind("<ButtonPress-1>", mouse_button_press)
+        canvas.bind("<ButtonRelease-1>", mouse_button_release)
+        root.mainloop()
+
+        # output result
+        with open("results.csv", "a") as myfile:
+            myfile.write(str(result))
 
 if __name__ == "__main__":
     main()
